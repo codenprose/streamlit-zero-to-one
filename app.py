43b870b002846f8577664b918cb3e233c6bd8386
dkh215 <daniel@crossbeam.com>
2023-05-11 22:37:37 -0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+st.markdown("""
+# Streamlit Playground
+### Hello, world! 👋
+""")
+
+st.write("This is my first Streamlit app!")
+
+st.sidebar.markdown("Streamlit Playground")
